977f5247c8e3373e48ed0c9b9d492c1f602f9eba
mk <v.a.stekolschikov@gmail.com>
2022-06-17 13:17:56 +0300

    added alert

--- v4/admin/src/containers/ExportPage/index.js
+++ v4/admin/src/containers/ExportPage/index.js
@@ -1,10 +1,10 @@
-import React, {memo, useEffect, useState} from 'react';
-import {BaseHeaderLayout} from '@strapi/design-system/Layout';
-import {Stack} from '@strapi/design-system/Stack';
-import {Box} from '@strapi/design-system/Box';
+import React, { memo, useEffect, useState } from 'react';
+import { BaseHeaderLayout } from '@strapi/design-system/Layout';
+import { Stack } from '@strapi/design-system/Stack';
+import { Box } from '@strapi/design-system/Box';
 import { Alert } from '@strapi/design-system/Alert';
-import {Typography} from '@strapi/design-system/Typography';
-import {getModels} from '../../utils/contentApis';
+import { Typography } from '@strapi/design-system/Typography';
+import { getModels } from '../../utils/contentApis';
 import ExportModel from "./ExportModel";
 
 const ExportPage = () => {
@@ -22,23 +22,33 @@ const ExportPage = () => {
 
     loadContentTypes();
   }, []);
+
+  const isContentTypes = !rightsError && models.length > 0
+  const isNoTables = !rightsError && models.length === 0
+
   return (
     <div>
-      <BaseHeaderLayout title="Export Content" subtitle="Export content into CSV format" as="h2"/>
+      <BaseHeaderLayout title="Export Content" subtitle="Export content into CSV format" as="h2" />
       <Stack size={4} padding={2}>
         {
-          !rightsError && <Box key="title" paddingLeft={9} paddingRight={9}>
+          isNoTables && <Box paddingLeft={9} paddingRight={9}>
+            <Alert closeLabel={""} title="" variant="default" onClose={() => { }}>You don't have tables or data!</Alert>
+          </Box>
+        }
+        {
+          rightsError && <Box paddingLeft={9} paddingRight={9}>
+            <Alert closeLabel={""} title="" variant="danger" onClose={() => { }}>Your rights are not enough, contact a user with super admin rights.</Alert>
+          </Box>
+        }
+        {
+          isContentTypes && <Box key="title" paddingLeft={9} paddingRight={9}>
             <Typography variant="beta">Content Types</Typography>
           </Box>
         }
         {
-          !rightsError && models.map((model) => (<ExportModel key={model.uid} model={model}/>))
+          !rightsError && models.map((model) => (<ExportModel key={model.uid} model={model} />))
         }
-        {
-          rightsError && <Box paddingLeft={9} paddingRight={9}>
-            <Alert closeLabel={""} title="Title" variant="danger" onClose={()=>{}}>Your rights are not enough, contact a user with super admin rights.</Alert>
-          </Box>
-        }
+
       </Stack>
     </div>
   );
